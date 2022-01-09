ca85d61df2e8908ec58e960efaf40338928fda45
Nastevych <58993797+Nastevych@users.noreply.github.com>
2022-01-09 16:00:27 +0200

    Update Calculator.java
    Add:
    - "You can't divide by zero." Exception;
    - "^" operation;
    - Exit operation .

--- Calculator.java
+++ Calculator.java
@@ -11,31 +11,37 @@ public class Calculator {
     }
 
     public void calculate() {
+        System.out.println("Simple Calculator :)");
+        System.out.println("How it works: number + <Enter> + number + <Enter> + operation.");
         while (!"q".equals(operation)) {
             try {
                 System.out.println(calculationStep());
             } catch (NumberFormatException er) {
-                System.err.println("Wrong input data!");
+                System.err.println("Wrong input data! You can use only numbers.");
             } catch (UnsupportedOperationException ex) {
-                System.err.println("The operation is not supported!");}
+                System.err.println("The operation is not supported! Only '+, -, *, /, ^' is available.");
+            } catch (ArithmeticException et){
+                System.err.println("You can't divide by zero.");
+            }
         }
-        }
+    }
 
         private double calculationStep () throws NumberFormatException {
+
             System.out.println("Enter first number:");
             double firstNumber = Double.parseDouble(scanner.nextLine());
 
             System.out.println("Enter second number:");
             double secondNumber = Double.parseDouble(scanner.nextLine());
 
-            System.out.println("Enter operation to perform (only '+, -, *, /' is available):");
+            System.out.println("Enter operation to perform or q for Quit. (only '+, -, *, /, ^' is available):");
             operation = scanner.nextLine();
 
             return performOperation(firstNumber, secondNumber);
         }
 
 
-    private double performOperation(double firstNumber, double secondNumber) {
+    private double performOperation (double firstNumber, double secondNumber) {
         double result;
         switch (operation){
             case "+":
@@ -48,13 +54,21 @@ public class Calculator {
                 result = multiply(firstNumber, secondNumber);
                 break;
             case "/":
+                if (secondNumber == 0){throw new ArithmeticException(String.format("Can't divide by zero", operation));
+                } else
                 result = divide(firstNumber, secondNumber);
                 break;
+            case "^":
+                result = pow(firstNumber, secondNumber);
+                break;
+            case "q":
+                System.out.println("You quit! Bye ;)");
+                System.exit(0);
             default:
                 throw new UnsupportedOperationException(String.format("The operation '%s' is not supported", operation));
         }
-        System.out.println("Result:");
-        return result;
+        System.out.println("Good job! Result for " + firstNumber + " " + operation + " " + secondNumber + " is");
+        return  result;
     }
 
     private double plus(double firstNumber, double secondNumber){
@@ -68,5 +82,8 @@ public class Calculator {
     }
     private  double divide(double firstNumber, double secondNumber){
         return  firstNumber / secondNumber;
+    }
+    private double pow (double firstNumber, double secondNumber){
+        return Math.pow(firstNumber,secondNumber);
     }
 }
